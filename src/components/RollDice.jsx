29ede77142ef56f7c518f2ebd5af396ba6dccbaa
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-08-28 03:01:46 +0530

    Made the Roll Dice component with functionality to display random dice between 1 to 6

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,7 +1,44 @@
-const RollDice = () => {
-  return (
-    <div>RollDice</div>
-  )
-}
+import { useState } from "react";
+import styled from "styled-components";
 
-export default RollDice
+const RollDice = () => {
+  const [currentDice, setCurrentDice] = useState(1);
+
+  //This function gives us a random Number between 1-6
+  const generateRandomNumber = (min, max) => {
+    console.log(Math.floor(Math.random() * (max - min) + min));
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  //This function calls the generateRandomNumber function and sets the random number in setCurrent Dice
+  const roleDice = () => {
+    const randomNumber = generateRandomNumber(1, 7);
+    setCurrentDice((prev) => randomNumber);
+  };
+
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={roleDice}>
+        <img src={`/images/dice/dice_${currentDice}.png`} alt="dice 1" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+export default RollDice;
+
+const DiceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 48px;
+
+  .dice {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 24px;
+  }
+`;
